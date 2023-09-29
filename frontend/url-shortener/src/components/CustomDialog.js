82f612335eb98199a45cf24a80358241d0f50b03
oscar.ng <oscar.ng@hactl.com>
2023-09-29 11:00:06 +0800

    added an UI layout for display the location info of the selected IP address

---
+++ frontend/url-shortener/src/components/CustomDialog.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+      padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+      padding: theme.spacing(1),
+    },
+  }));
+
+function CustomDialog({open, handleClose, ip_addr}) {
+    return(
+        <BootstrapDialog
+            onClose={handleClose}
+            aria-labelledby="customized-dialog-title"
+            open={open}
+        >
+            <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                The location of the IP address
+            </DialogTitle>
+            <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+            }}
+            >
+            <CloseIcon />
+            </IconButton>
+            <DialogContent dividers>
+            <Typography gutterBottom>
+                Target: {ip_addr}
+            </Typography>
+            <Typography gutterBottom>
+                City: Taipei
+            </Typography>
+            <Typography gutterBottom>
+                Country: Taiwan
+            </Typography>
+            <Typography gutterBottom>
+                Latitude: 25.0478
+            </Typography>
+            <Typography gutterBottom>
+                Longitude: 121.5318
+            </Typography>
+            </DialogContent>
+            <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+                OK
+            </Button>
+            </DialogActions>
+        </BootstrapDialog>
+    );
+}
+
+export default CustomDialog;
--- frontend/url-shortener/src/pages/AccessLog.js
+++ frontend/url-shortener/src/pages/AccessLog.js
@@ -5,16 +5,28 @@ import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import { getAccessLog } from '../services/AccessLogService';
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'ip_addr', headerName: 'IP address', width: 200 },
-  { field: 'access_time', headerName: 'Access Time', width: 200 },
-];
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import { Link } from 'react-router-dom';
+import CustomDialog from '../components/CustomDialog';
 
 function AccessLog() {
   const [accessLog, setAccessLog] = useState(null);
   const [trackingCode, setTrackingCode] = useState('');
+  const [ip_addr, setIp_addr] = useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'ip_addr', headerName: 'IP address', width: 200,
+      renderCell: (params) => (
+        <Link href="#" onClick={() => handleClickOpen(params.value)} style={{ textDecoration: 'none', color: 'inherit' }}>
+          {params.value}
+        </Link>
+      ),
+    },
+    { field: 'access_time', headerName: 'Access Time', width: 200 },
+  ];
 
   const handleTrackingCodeChange = (event) => {
     setTrackingCode(event.target.value);
@@ -28,6 +40,14 @@ function AccessLog() {
 
   const SearchButtonClick = () => {
     fetchData();
+  };
+
+  const handleClickOpen = (ip_addr) => {
+    setIp_addr(ip_addr);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -66,6 +86,8 @@ function AccessLog() {
           <div>Please input a tracking code to get the access record.</div>
         )}
       </div>
+
+      <CustomDialog open={open} handleClose={handleClose} ip_addr={ip_addr} />
     </div>
   );
 }
